8f2ffa68feab93d380c3e35c2e777535ae8775f6
Victor REMOND <victor96@hotmail.fr>
2017-02-28 16:49:49 +0100

    ..

--- Code/src/Base/Plateau.java
+++ Code/src/Base/Plateau.java
@@ -32,10 +32,21 @@ public class Plateau {
 	public void poserPiece(Piece p, int px, int py)
 	{
 		//Pose une piece aux coordonnees voulues
+		if(positionPossible(p, px, py))
+		{
+			//On peut poser la piece
+			for()
+		}
 	}
 	
 	public boolean positionPossible(Piece p, int px, int py)
 	{
+		//Si la piece sort du plateau
+		if((px < 0) || ((px + p.Hauteur()) > m_tab.length))
+			return false;
+		if((py < 0) || ((py + p.Largeur() > m_tab[0].length)))
+			return false;
+		//On verifie qu'il n'y ait pas deux cases occupees superposees
 		for(int i = 0; (i < p.Hauteur()) && (i < m_tab.length); i++)
 		{
 			for(int j = 0; (j < p.Largeur()) && (j < m_tab[0].length); j++)
