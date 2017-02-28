bbc392e72ff20548434ac127fa6dc79e8d3866af
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2017-02-28 17:19:39 +0100

    .

---
+++ Tetris/src/Base/Case.java
@@ -0,0 +1,21 @@
+package Base;
+
+public class Case 
+{	
+	protected int m_couleur;
+
+	public Case() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getCouleur()
+	{
+		return this.m_couleur;
+	}
+	
+	public void setCouleur(int couleur)
+	{
+		this.m_couleur = couleur;
+	}
+}
