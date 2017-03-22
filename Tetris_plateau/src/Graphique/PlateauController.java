30601f7e4abbe70587e099ff2d7f0a5cba11f5b5
Victor REMOND <victor96@hotmail.fr>
2017-03-22 17:17:53 +0100

    ..

--- Tetris_plateau/src/Graphique/PlateauController.java
+++ Tetris_plateau/src/Graphique/PlateauController.java
@@ -1,27 +1,26 @@
 package Graphique;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import Main.*;
 
+import Base.Plateau;
+
 import java.util.ArrayList;
 
-import Base.*;
+import Base.Case;
 
 public class PlateauController
 {
 
-	private ObservableList<Node> m_rectListe;
-	private Rectangle m_r;
-
-	private Main _main;
+	protected Main m_main;
+	protected int m_h, m_l;
+	protected ObservableList<Case> m_cases = FXCollections.observableArrayList();
+	protected Rectangle[][] m_rect;
 
 	@FXML
 	private GridPane grille;
@@ -34,16 +33,12 @@ public class PlateauController
 	@FXML
     private void initialize()
 	{
-		m_rectListe = grille.getChildren();
-		for(int i = 0; i < m_rectListe.size(); i++)
-		{
-			m_r = (Rectangle)m_rectListe.get(i);
-		}
+		//
 	}
 
 	public void setMain(Main main)
 	{
-		this._main = main;
-		grille.getChildren().setAll((Node)main.getPlateau());
+		this.m_main = main;
+		//
 	}
 }
