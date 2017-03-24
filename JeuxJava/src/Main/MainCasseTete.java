81eb7a6743ad4e805d1faa7e2c59cd17db4b064e
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2017-03-24 17:51:08 +0100

    .

--- JeuxJava/src/Main/MainCasseTete.java
+++ JeuxJava/src/Main/MainCasseTete.java
@@ -3,7 +3,11 @@ package Main;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import Main.*;
+
+public class MainCasseTete extends Application {
+
+
 
 	@Override
 	public void start(Stage primaryStage) {
--- JeuxJava/src/Modele/CasseTete.java
+++ JeuxJava/src/Modele/CasseTete.java
@@ -1,5 +1,31 @@
 package Modele;
 
+import Base.*;
+
 public class CasseTete {
+
+	public Plateau _p;
+
+	public CasseTete()
+	{
+		_p = new Plateau(10,10);
+	}
+
+	public Plateau getPlateau()
+	{
+		return _p;
+	}
+
+	public void setPlateau(Plateau p)
+	{
+		_p = p;
+	}
+
+	public void jouer()
+	{
+
+	}
+
+
 
 }
