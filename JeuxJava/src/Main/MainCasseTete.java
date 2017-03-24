beb8d75afae690eb0826fec87c24a395f9b97f72
BastienJacoud <bastien.jacoud@etu.univ-lyon1.fr>
2017-03-24 17:57:56 +0100

    .

--- JeuxJava/src/Main/MainCasseTete.java
+++ JeuxJava/src/Main/MainCasseTete.java
@@ -1,16 +1,30 @@
 package Main;
 
 import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import Main.*;
+import Modele.CasseTete;
 
 public class MainCasseTete extends Application {
 
+	private Stage m_primaryStage;
+	private BorderPane m_rootLayout;
+	public Main m;
+	public CasseTete c;
 
+	public MainCasseTete()
+	{
+		m = new Main();
+		c = new CasseTete();
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
+		m_primaryStage = primaryStage;
+		m_primaryStage.setTitle("Casse Tête");
+		m.start(primaryStage);
 
 	}
 
